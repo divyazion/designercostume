ecf4fda900e1f4933ab79878caa4a4bfa7f00b8f
Ubuntu <ubuntu@ip-172-31-14-58.us-east-2.compute.internal>
2021-10-11 18:57:30 +0000

    adding vm files for ec2 with modules

---
+++ terraform-aws-ec2-withmodules/module/vm/variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  type    = string
+}
+
+variable "ami_id" {
+  type = map
+  default = {
+    us-east-2    = "ami-00399ec92321828f5"
+    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+    eu-central-1 = "ami-9787h5h6nsn75gd33"
+  }
+}
+
+variable "instance_type" {
+  type    = string
+}
+variable "key_name" {
+  type    = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "vpc_security_group_ids" {
+  type = string
+}
+
